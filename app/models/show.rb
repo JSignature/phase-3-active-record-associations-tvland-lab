5edbd5d839b882ab66d9a35697dd37cdc6f38015
Jimmy Andreas <jimmysignature@gmail.com>
2022-08-03 20:56:00 -0400

    Completed assignment

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,15 @@
 class Show < ActiveRecord::Base
+    has_many :characters
+    belongs_to :network
+    has_many :actors, through: :characters
+
+
+    def actors_list
+        act = ["#{self.actors[0].first_name} #{self.actors[0].last_name}"]
+       
+
+    end
+
+
   
 end
